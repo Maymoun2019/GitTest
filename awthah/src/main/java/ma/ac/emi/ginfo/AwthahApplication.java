4408c85a65fd0bf25774168893384b60fb176c5e
Maymoun2019 <maymoun.elfahim@gmail.com>
2021-03-17 00:53:30 +0100

    first comm

---
+++ awthah/src/main/java/ma/ac/emi/ginfo/AwthahApplication.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.ginfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwthahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwthahApplication.class, args);
+	}
+
+}
---
+++ awthah/src/test/java/ma/ac/emi/ginfo/AwthahApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ac.emi.ginfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwthahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
